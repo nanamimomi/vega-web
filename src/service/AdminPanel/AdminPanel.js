e4b5ff6078dc88fcdec3828db3393fa85312e85b
Callum <66440161+ccurtis3@users.noreply.github.com>
2022-06-11 20:39:23 -0700

    Setup Docker networking

--- src/service/AdminPanel/AdminPanel.js
+++ src/service/AdminPanel/AdminPanel.js
@@ -1,13 +1,13 @@
 import {doPostFile, doGet} from '../BaseAPI.js';
 
 export function fetchuser(token){
-	return doGet("http://localhost:8000/api/venus/admin/getusers", token)
+	return doGet(process.env.REACT_APP_API_URL + "/api/venus/admin/getusers", token)
 }
 
 export function enableAccount(username,token){
-	return doGet("http://localhost:8000/api/venus/admin/enableuser?enable=true&username="+username, token)	
+	return doGet(process.env.REACT_APP_API_URL + "/api/venus/admin/enableuser?enable=true&username="+username, token)
 }
 
 export function changeAccountRole(username, role, token){
-	return doGet("http://localhost:8000/api/venus/admin/changerole?username="+username+"&role="+role, token)
+	return doGet(process.env.REACT_APP_API_URL + "/api/venus/admin/changerole?username="+username+"&role="+role, token)
 }

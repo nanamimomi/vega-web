31b8672dbbc7109527b9b48d8ab65261a934a93d
RickyHuang19 <rickhuang5@gmail.com>
2022-06-12 19:00:14 -0700

    Adding 1 Dummy Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {UserContext} from "./auth/UserProvider";
+import UserAccount from "./components/pages/UserAccount";
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Dummy Test', () => {
+  const x = 0;
+  expect(0);
+})

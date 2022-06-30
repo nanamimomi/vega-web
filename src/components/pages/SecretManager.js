61c91211f5eb547dbe33af45442add2bd1b68e01
Callum <66440161+ccurtis3@users.noreply.github.com>
2022-06-29 21:02:39 -0700

    Add date filters and button to create secrets

--- src/components/pages/SecretManager.js
+++ src/components/pages/SecretManager.js
@@ -1,9 +1,34 @@
+import React from "react";
 import SimplePageLayout from "../templates/SimplePageLayout";
+import DateTimeRangePicker from "../UI/molecules/DateTimeRangePicker";
 
 const SecretManager = () => {
+
+    const addSecret = () => {
+        console.log("Add Secret Requested");
+    }
+
+    const dateTimeFilterChange = (start, end) => {
+        console.log("Date Time Filter Changed: start = " + start + ", end = " + end);
+    }
+
+    const defaultEndOfDateTimeFilter = new Date();
+    const defaultStartOfDateTimeFilter = new Date();
+    defaultStartOfDateTimeFilter.setMonth(defaultEndOfDateTimeFilter.getMonth() - 1);
+    const maxOfDateTimeFilter = new Date();
+
     return (
         <SimplePageLayout>
-            <p>Welcome to the secret manager page!</p>
+            <h1>Secrets</h1>
+            <div className={"d-flex justify-content-between"}>
+                <button onClick={addSecret}>Add Secret</button>
+                <DateTimeRangePicker
+                    start={defaultStartOfDateTimeFilter}
+                    end={defaultEndOfDateTimeFilter}
+                    max={maxOfDateTimeFilter}
+                    onChange={dateTimeFilterChange}
+                />
+            </div>
         </SimplePageLayout>
     );
 }

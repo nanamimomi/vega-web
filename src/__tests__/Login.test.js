103611449fa2dfffaadf7b3393b6944bd8493d26
Broondan <bwadey@uvic.ca>
2022-07-01 16:23:36 -0700

    Adding associated user story to this login test.

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -2,7 +2,15 @@ import { render, screen } from '@testing-library/react'
 import user from "@testing-library/user-event"
 import Login from '../components/pages/Login'
 
+// Feature: Login Functionality
+// Description: As a user, I want to login to the system using my email address and password, so I can access restricted features.
 describe('UI-test tutorial', () => {
+
+  // Scenario: User provides correct credentials.
+  // Given I am on the “login” page
+  // When I give the correct username password combination
+  // Then I am taken to the “home” page
+  // And I am logged in
   test('test3', () => {
     render(<Login />);
 
@@ -16,4 +24,9 @@ describe('UI-test tutorial', () => {
     // const admin_navbar = screen.getByRole("navbar", {name: "Submit"});
     // expect(admin_navbar).toHaveTextContent("Admin");
   })
+
+  //Scenario: User provides incorrect credentials.
+  //Given I am on the “login” page
+  //When I type in the wrong username password combination
+  //Then I see an error message “Invalid username or password.”
 })

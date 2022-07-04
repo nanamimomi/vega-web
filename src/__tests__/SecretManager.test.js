f9c5dc8329fc1fe8537466be48c3bc4827822ac1
Broondan <bwadey@uvic.ca>
2022-07-03 22:05:30 -0700

    Created new test files, and finished a test suite for the VenusNavBar changes. Had to extract dependancy from UserProvider.js in order to test.

---
+++ src/__tests__/SecretManager.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import user from "@testing-library/user-event"
+import Login from '../components/pages/Login'
+import SecretManager from "../components/pages/SecretManager";
+
+// Feature:
+// Description:
+describe('UI-test tutorial', () => {
+
+  // Scenario:
+  // Given
+  // When
+  // Then
+  test('test4', () => {
+    render(<SecretManager />);
+
+  })
+})
